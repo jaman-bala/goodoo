dfb5556447f31bdf821407eabdd92ae2a07dd061
jamanbala_it <ermekoffdastan@yandex.ru>
2022-12-03 13:19:11 -0600

    edit makefile

--- main/settings.py
+++ main/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("POSTGRES_DB",default='goodoo_db'),
-        'USER' : os.getenv("POSTGRES_USER",default='goodoo_user'),
-        'PASSWORD' : os.getenv("POSTGRES_PASSWORD",default='goodoo2022!'),
-        'HOST' : os.getenv("POSTGRES_HOST",default='127.0.0.1'),
-        'PORT' : os.getenv("POSTGRES_PORT", default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -125,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

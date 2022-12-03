2fd5abc819599ecb0ac77f0cd2c387f88dd6b82f
jamanbala_it <ermekoffdastan@yandex.ru>
2022-12-03 13:29:38 -0600

    edit db

--- main/settings.py
+++ main/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("POSTGRES_DB",default='goodoo_db'),
+        'USER' : os.getenv("POSTGRES_USER",default='goodoo_user'),
+        'PASSWORD' : os.getenv("POSTGRES_PASSWORD",default='goodoo2022!'),
+        'HOST' : os.getenv("POSTGRES_HOST",default='127.0.0.1'),
+        'PORT' : os.getenv("POSTGRES_PORT", default='5432'),
     }
 }
 

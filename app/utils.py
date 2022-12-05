d134202813394f7585ea50e3ab0f79f15ecf0e96
jamanbala_it <ermekoffdastan@yandex.ru>
2022-12-05 04:51:51 -0600

    edit utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 def get_token():
-    URL = "http://10.12.20.65/api/licences"
+    URL = "http://10.12.20.65:9099/api/licences"
     response = requests.get(URL, auth=HTTPBasicAuth("admin", "admin"))
     if response.status_code == 200:
         data = response.json()
